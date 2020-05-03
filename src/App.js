3c679f2ae9ddf7d6704044258f073ec5ffbbc08a
Davide Vernizzi <davide@vernizzis.it>
2020-05-03 13:27:13 +0200

    feat(): add basic demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function useInput({ type, defaults }) {
+  const [value, setValue] = useState(defaults);
+  const input = <input value={value} onChange={e => setValue(e.target.value)} type={type} autofocus />;
+  return [value, input];
+}
+
+const Themes = ({setTheme}) => {
+  const themeList = [
+    'spaceGrey',
+    'shiny',
+    'coral'
+  ]
+
+  return <ul> {themeList.map(t => {
+    const _setTheme = () => setTheme(t)
+    return <li onClick={_setTheme}>{t}</li>
+  }
+  )} </ul>
+}
+
 function App() {
+  const [_, userInput] = useInput({ type: "text", defaults: "Your cover here" });
+  const [ theme, setTheme ] = useState('spaceGrey');
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={theme} >
+      {userInput}
+      < Themes setTheme={setTheme}/>
     </div>
   );
 }
