1b868098d2583d127f7a00a3e526a6a894ae056a
Davide Vernizzi <davide@vernizzis.it>
2020-05-03 17:24:26 +0200

    Feat(): add themes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,49 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function useInput({ type, defaults }) {
   const [value, setValue] = useState(defaults);
-  const input = <input value={value} onChange={e => setValue(e.target.value)} type={type} autofocus />;
+  const input = <input value={value} onChange={e => {
+    document.title = `Cver - ${e.target.value}`
+    setValue(e.target.value)
+  }} type={type} autoFocus />;
   return [value, input];
 }
 
 const Themes = ({setTheme}) => {
-  const themeList = [
-    'spaceGrey',
-    'shiny',
-    'coral'
-  ]
+  const themeList = {
+    'Space Grey': 'spaceGrey',
+    'Shiny': 'shiny',
+    'Coral': 'coral'
+  }
 
-  return <ul> {themeList.map(t => {
-    const _setTheme = () => setTheme(t)
+
+  return <ul> {Object.keys(themeList).map(t => {
+    const _setTheme = () => setTheme(themeList[t])
     return <li onClick={_setTheme}>{t}</li>
   }
   )} </ul>
 }
 
 function App() {
-  const [_, userInput] = useInput({ type: "text", defaults: "Your cover here" });
+  const [ cverText, cverTextInput] = useInput({ type: "text", defaults: "Your cover here" });
   const [ theme, setTheme ] = useState('spaceGrey');
+
+  console.log(document.body.style)
   
   return (
     <div className={theme} >
-      {userInput}
-      < Themes setTheme={setTheme}/>
+      <div className='input'>
+        {cverTextInput}
+      </div>
+      <div className='themes'>
+        Select theme:
+        <Themes setTheme={setTheme}/>
+      </div>
+      <div className='footer'>
+        <p>Why?</p>
+        <p>Made with ❤️ by <a href='https://www.vernizzis.it/'>Dave</a></p>
+      </div>
     </div>
   );
 }
