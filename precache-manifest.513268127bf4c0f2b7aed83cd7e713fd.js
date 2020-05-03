ba96de708ceab5db0c8d930abad99c4bb7d7cd8d
Davide Vernizzi <davide@vernizzis.it>
2020-05-03 18:28:52 +0200

    Updates

---
+++ precache-manifest.513268127bf4c0f2b7aed83cd7e713fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673aae4296b02ca046ae799e8619c5bc",
+    "url": "/cver/index.html"
+  },
+  {
+    "revision": "31dc693bb7327933a578",
+    "url": "/cver/static/css/main.2cb92673.chunk.css"
+  },
+  {
+    "revision": "e571331f5fb1d175c2cc",
+    "url": "/cver/static/js/2.6491a9c1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cver/static/js/2.6491a9c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31dc693bb7327933a578",
+    "url": "/cver/static/js/main.7af0f624.chunk.js"
+  },
+  {
+    "revision": "66b9f22cff21dd6e982f",
+    "url": "/cver/static/js/runtime-main.4a8ed5bc.js"
+  },
+  {
+    "revision": "b98cb24f788aa1254a29cdce5cf1534a",
+    "url": "/cver/static/media/Desktop.b98cb24f.png"
+  }
+]);
